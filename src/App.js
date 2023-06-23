5a6d6703a6b8f4801f039abe575eeb562746449e
sekkiyah <seth_hinshaw@hotmail.com>
2023-06-22 21:16:39 -0500

    initial project buildout

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+const App = () => {
+  return (
+    <>
+      <Container>
+        <Routes>
+          <Route path='/' element={<></>} />
+        </Routes>
+      </Container>
+    </>
+  );
+};
+
+export default App;
