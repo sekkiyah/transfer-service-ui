7b5de773853f9629f5dad1ac9ac4d4a2e187b8a9
sekkiyah <seth_hinshaw@hotmail.com>
2023-08-20 14:42:37 -0500

    renamed TransferDataForm component for purpose clarity

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ export { default as Dashboard } from './Dashboard';
 export { default as TransferForm } from './TransferForm';
 export { default as CreateTransfer } from './CreateTransfer';
 export { default as TransferProtocolForm } from './TransferProtocolForm';
-export { default as TransferDataForm } from './TransferDataForm';
+export { default as OutboundTransferDataForm } from './OutboundTransferDataForm';
 export { default as TransferListTable } from './TransferListTable';
