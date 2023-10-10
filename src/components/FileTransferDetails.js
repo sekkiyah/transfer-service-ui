5c763dd51e848a73dae9b366fc376f7174813f64
sekkiyah <seth_hinshaw@hotmail.com>
2023-10-09 23:04:10 -0500

    built initial FileTransferDetails layout in two different possibilities

--- src/components/FileTransferDetails.js
+++ src/components/FileTransferDetails.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Accordion, Button, Form, Modal, Table } from 'react-bootstrap';
 
 const FileTransferDetails = ({ file }) => {
   const [showModal, setShowModal] = useState(false);
@@ -10,14 +10,59 @@ const FileTransferDetails = ({ file }) => {
         {file.transferLogs[0].status}
       </Button>
 
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
+      <Modal size='lg' show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>File Name</Modal.Title>
+          <Modal.Title>{file.name}</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          <h3>TBD</h3>
+          {file.transferLogs &&
+            file.transferLogs.length &&
+            file.transferLogs.map((transferLog, index) => {
+              const runCount = file.transferLogs.length - index;
+              const lineCount = transferLog.logMessage.split('\n').length;
+              console.log(transferLog.logMessage, lineCount);
+
+              return (
+                <Accordion key={index}>
+                  <Accordion.Header className=''>
+                    Attempt: {runCount} - Ran: {transferLog.transferDate} - By: {transferLog.transferredBy}
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <Form.Control as='textarea' disabled value={transferLog.logMessage} rows={lineCount} />
+                  </Accordion.Body>
+                </Accordion>
+              );
+            })}
         </Modal.Body>
+
+        {/* <Modal.Body as={Table} size='sm' className='text-center'>
+          <thead>
+            <tr>
+              <th>Attempt</th>
+              <th>Ran By</th>
+              <th>Status</th>
+              <th>Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            {file.transferLogs && file.transferLogs.length ? (
+              file.transferLogs.map((transferLog, index) => {
+                index++;
+                return (
+                  <tr>
+                    <td>{index}</td>
+                    <td>{transferLog.transferredBy}</td>
+                    <td>{transferLog.status}</td>
+                    <td>{transferLog.logMessage}</td>
+                  </tr>
+                );
+              })
+            ) : (
+              <></>
+            )}
+          </tbody>
+        </Modal.Body> */}
       </Modal>
     </>
   );
